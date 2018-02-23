1eeb8663fd9fb1a17719b594e7421d62b0af2cc2
Seorenn <hirenn@gmail.com>
2018-02-23 18:31:28 +0900

    Starting project

---
+++ SRStartupLauncher/SRStartupLauncher.h
@@ -0,0 +1,19 @@
+//
+//  SRStartupLauncher.h
+//  SRStartupLauncher
+//
+//  Created by Seorenn on 2018. 2. 23..
+//  Copyright © 2018년 Seorenn. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for SRStartupLauncher.
+FOUNDATION_EXPORT double SRStartupLauncherVersionNumber;
+
+//! Project version string for SRStartupLauncher.
+FOUNDATION_EXPORT const unsigned char SRStartupLauncherVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SRStartupLauncher/PublicHeader.h>
+
+
---
+++ Source/SRStartupLauncher.swift
@@ -0,0 +1,13 @@
+//
+//  SRStartupLauncher.swift
+//  SRStartupLauncher
+//
+//  Created by Seorenn on 2018. 2. 23..
+//  Copyright © 2018년 Seorenn. All rights reserved.
+//
+
+import Cocoa
+
+class SRStartupLauncher: NSObject {
+    // TODO
+}
